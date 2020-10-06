3c41836521f142001799797da2a25942aaf1bfbc
Jennifer Currie <renestl@gmail.com>
2020-10-06 04:03:24 -0500

    Add matrix addition

--- processor/processor.py
+++ processor/processor.py
@@ -15,6 +15,7 @@ b_rows, b_cols = input().split()
 matrix_b = init_matrix(int(b_rows))
 
 if a_rows == b_rows and a_cols == b_cols:
-    print('they match')
+    result = [[matrix_a[row][col] + matrix_b[row][col] for col in range(len(matrix_a[0]))] for row in range(len(matrix_a))]
+    [print(' '.join(map(str, result[row]))) for row in range(len(result))]
 else:
     print('ERROR')
